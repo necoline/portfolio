b5d34599b8f0d306c145585641c96470f8859556
Necoline <necoline@developmentseed.org>
2021-07-14 20:14:26 -0400

    Add header font and style

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,11 +2,11 @@ import * as React from "react"
 
 export default function Footer() {
   return (
-    <footer>
-        <hr/>
-        <h3>Learn about my creative process</h3>
-        <p>Click the button below to view the story map and design that supported the creation of this website.</p>
-        <button>Learn More</button>
-      </footer>
+    <footer className="global-body-wrapper">
+      <hr/>
+      <h3>Learn about my creative process</h3>
+      <p>Click the button below to view the story map and design that supported the creation of this website.</p>
+      <button>Learn More</button>
+    </footer>
   )
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,26 +5,17 @@ import Footer from "./footer"
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
+  
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
+      <header className="global-header">
+        <h1 className="main-heading">
+        <Link to="/">{title}</Link>
+        </h1>
+        <hr></hr>
+      </header>
+      <main className="global-body-wrapper">{children}</main>
       <Footer/>
     </div>
   )
